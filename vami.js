b76725f0cfc0127bb488dcc51babf3fc8cfb3b45
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2024-11-18 15:57:33 +0530

    events@2

---
+++ vami.js
@@ -0,0 +1,187 @@
+// // const hoverDiv = document.getElementById('hoverDiv');
+// // hoverDiv.addEventListener('mouseover', () => {
+// //   hoverDiv.style.backgroundColor = 'lightcoral';
+// // });
+// // hoverDiv.addEventListener('mouseout', () => {
+// //   hoverDiv.style.backgroundColor = 'lightblue';
+// // });
+
+
+// // const textInput = document.getElementById('textInput');
+// // textInput.addEventListener('keydown', (event) => {
+// //   console.log(`You pressed: ${event.key}`);
+// // });
+
+
+// // const GParent=document.getElementById("grand");
+// // GParent.addEventListener("click",(e)=>{
+// //     e.stopPropagation();
+// //     console.log("grand parent clikced");
+// // })
+
+// // const P=document.getElementById("parent");
+// // P.addEventListener("click",(e)=>{
+// //     e.stopPropagation();
+// //     console.log(" parent clikced");
+// // })
+
+// // const C=document.getElementById("child");
+
+// // C.addEventListener("click",(e)=>{
+// //     e.stopPropagation();
+// //     console.log("child clikced");
+// // })
+
+
+
+
+
+// // const GParent=document.getElementById("grand");
+// // GParent.addEventListener("click",(e)=>{
+// //     e.stopPropagation()
+// //     console.log("grand parent clikced");
+// // })
+
+// // const P=document.getElementById("parent");
+// // P.addEventListener("keydown",(e)=>{
+// //     e.stopPropagation()
+// //     console.log(" parent clikced");
+// // })
+
+// // const C=document.getElementById("child");
+
+// // C.addEventListener("mouseover",(e)=>{
+// //     e.stopPropagation()
+// //     console.log("child clikced");
+// // })
+
+
+
+
+// // const GParent=document.getElementById("grand");
+// // GParent.addEventListener("click",(e)=>{
+// //     e.stopPropagation();
+// //     setTimeout(()=>{
+// //         console.log("grand parent clikced");
+// //         GParent.style.backgroundColor="lightblue"
+// //     },3000)
+// // },false)
+
+// // const P=document.getElementById("parent");
+// // P.addEventListener("click",(e)=>{
+// //     e.stopPropagation();
+// //     setTimeout(()=>{
+// //         console.log("parent clikced");
+// //         P.style.backgroundColor="yellow"
+// //     },2000)
+// // },false)
+
+// // const C=document.getElementById("child");
+
+// // C.addEventListener("click",(e)=>{
+// //     e.stopPropagation();
+// //     setTimeout(()=>{
+// //         console.log("child clikced");
+// //         C.style.backgroundColor="purple"
+// //     },1000)
+// // },false)
+
+
+
+
+// // const GParent=document.getElementById("grand");
+// // GParent.addEventListener("click",(e)=>{
+// //     e.stopPropagation();
+// //     console.log("grand parent clikced");
+// // })
+
+// // const P=document.getElementById("parent");
+// // P.addEventListener("click",(e)=>{
+// //     e.stopPropagation();
+// //     console.log(" parent clikced");
+// // })
+
+// // const C=document.getElementById("child");
+
+// // C.addEventListener("click",(e)=>{
+// //     e.stopPropagation();
+// //     console.log("child clikced");
+// // })
+
+
+
+// // const GParent=document.getElementById("grand");
+// // GParent.addEventListener("click",(e)=>{
+// //     setTimeout(()=>{
+// //         console.log("grand parent clikced");
+// //         GParent.style.backgroundColor="red"
+// //     },1000)
+// // },true)
+
+// // const P=document.getElementById("parent");
+// // P.addEventListener("click",(e)=>{
+// //     e.stopPropagation()
+// //    setTimeout(()=>{
+// //     console.log(" parent clikced");
+// //     P.style.backgroundColor="orange"
+// //    },2000)
+// // },true)
+
+// // const C=document.getElementById("child");
+
+// // C.addEventListener("click",(e)=>{
+// //    setTimeout(()=>{
+// //         C.style.backgroundColor="green"
+// //         console.log("child clikced");
+// //    },3000)
+// // },true)
+
+
+
+
+// // const GParent=document.getElementById("grand");
+// // GParent.addEventListener("click",(e)=>{
+// //     setTimeout(()=>{
+// //         console.log("grand parent clikced");
+// //         GParent.style.backgroundColor="red"
+// //     },1000)
+// // },true)
+
+// // const P=document.getElementById("parent");
+// // P.addEventListener("click",(e)=>{
+// //    setTimeout(()=>{
+// //     console.log(" parent clikced");
+// //     P.style.backgroundColor="orange"
+// //    },2000)
+// // },false)
+
+// // const C=document.getElementById("child");
+
+// // C.addEventListener("click",(e)=>{
+// //    setTimeout(()=>{
+// //         C.style.backgroundColor="green"
+// //         console.log("child clikced");
+// //    },3000)
+// // },true)
+
+
+
+
+// const GParent=document.getElementById("grand");
+// GParent.addEventListener("click",(e)=>{
+//   e.stopPropagation()
+//     console.log("grand parent clikced");
+// },false)
+
+// const P=document.getElementById("parent");
+// P.addEventListener("click",(e)=>{
+//     // e.stopPropagation();
+//     console.log(" parent clikced");
+// },true)
+
+// const C=document.getElementById("child");
+
+// C.addEventListener("click",(e)=>{
+//     e.stopPropagation();
+//     console.log("child clikced");
+// },false)
